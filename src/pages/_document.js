96e8ca23352cdb085c90f28fa2cbc2b01124721e
Johary <joharyandrianjafimanohisolo@gmail.com>
2023-09-23 18:04:25 +0300

    chargement auto, aléger image de fond

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,29 @@
 /* eslint-disable @next/next/google-font-display */
+import { useEffect } from "react";
 import { Html, Head, Main, NextScript } from "next/document";
+import { currentCategoryAtom } from "@atoms/categoryAtom";
+import { getAll } from "@services/product";
+import { useSetRecoilState } from "recoil";
+import { productsAtom } from "@atoms/productAtom";
 
 export default function Document() {
+  const setProductsAtom = useSetRecoilState(productsAtom);
+  const setCurrentCategory = useSetRecoilState(currentCategoryAtom);
+  useEffect(() => {
+    setCurrentCategory({ caption: "All Stones", value: "list" });
+    getAll().then((products) => {
+      const p = products.map((product) => {
+        return {
+          ...product,
+          url:
+            product.nomProduit.toLowerCase().replaceAll(" ", "-") +
+            "-" +
+            product.id,
+        };
+      });
+      setProductsAtom(p);
+    });
+  }, [setProductsAtom, setCurrentCategory]);
   return (
     <Html lang="en">
       <Head>
